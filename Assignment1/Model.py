47d50eb751bf51d93221f9ba0e0a782aadd32761
Ankitchawla3123 <ankitchawla3123@gmail.com>
2026-04-28 08:11:47 +0530

    Add files via upload

--- Assignment1/Model.py
+++ Assignment1/Model.py
@@ -8,8 +8,12 @@ from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTENC
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.datasets import load_breast_cancer
+from imblearn.over_sampling import SMOTEN
+
 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier, OutputCodeClassifier
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold, cross_val_score
@@ -27,6 +31,148 @@ from sklearn.metrics import (
     confusion_matrix
 )
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from abc import ABC, abstractclassmethod
+
+class BaseClass(ABC):
+    def __init__(self,X,y,numcol,catcol):
+        self.X = X
+        self.y = y
+        self.numcol = numcol
+        self.catcol = catcol
+        self.simulation_result_n={}
+        self.simulation_result_one={}
+        self.data_sim_one={}
+        self.data_sim_n={}
+        self.validate_and_assign_columns()
+    
+    def validate_and_assign_columns(self):
+        numeric_cols=[]
+        cat_cols=[]
+        for Num,Cat,Col in zip(self.numcol,self.catcol,self.X.columns.tolist()):
+            if(Num==1 and Cat==1):
+                raise Exception("Column could not be both cat and num.")
+
+            if(Num==1):
+                numeric_cols.append(Col)
+            elif(Cat==1):
+                cat_cols.append(Col)
+            else:
+                raise Exception("A column has to be cat/numeric.")
+        self.numcol=numeric_cols
+        self.catcol=cat_cols
+    
+       
+    def preprocess(self, EncodeCat=False,EncodeLabel=False,seed=42,sampling=2):
+        
+        '''
+        other logic would be apply one hot and std scaler the in last apply smote
+        then it would have 1 final pipline 
+        
+        '''
+
+        '''
+        Docstring for preprocess
+        
+        :param EncodeCat: by default false
+        :param EncodeLabel: by default false
+        :param seed: 
+        :param sampling: 0 for no sampling, 1 for undersampling, 2 for oversampling
+        '''
+        
+        # label encoding
+        if EncodeLabel:
+            self.label_encoder = LabelEncoder()
+            y_encoded = self.label_encoder.fit_transform(self.y)
+        else:
+            y_encoded = self.y
+        
+        X_train,X_test,y_train,y_test=train_test_split(self.X,y_encoded,random_state=seed,test_size=0.3, stratify=y_encoded)
+
+        
+        # standardization
+        numeric_pipeline = Pipeline(steps=[
+            ("imputer", SimpleImputer(strategy="mean")),
+            ("scaler", StandardScaler())
+        ])
+        # numeric_processor = make_pipeline(
+        #     SimpleImputer(strategy="mean"),
+        #     StandardScaler()
+        #     )
+        
+        scale_transformer = ColumnTransformer(
+            transformers=[
+            ("num", numeric_pipeline, self.numcol)
+            ],
+            remainder="passthrough"   # keep categorical untouched
+            )
+        
+        X_train_scaled = scale_transformer.fit_transform(X_train)
+        X_test_scaled  = scale_transformer.transform(X_test)
+        
+        # smotenc
+        original_cols = list(self.X.columns)
+        remainder_cols = [col for col in original_cols if col not in self.numcol]
+
+        cat_indices = [
+            len(self.numcol) + remainder_cols.index(cat_col)
+            for cat_col in self.catcol
+        ]
+
+        if sampling==0:
+            
+            X_train_res,y_train_res=X_train_scaled,y_train
+            
+        elif sampling==1:
+
+            rus = RandomUnderSampler(sampling_strategy='majority')
+            X_train_res, y_train_res = rus.fit_resample(X_train_scaled, y_train)
+            
+        elif sampling==2:
+            if (1 in self.numcol):
+                
+                smote = SMOTENC(
+                    categorical_features=cat_indices
+                    )
+                X_train_res, y_train_res = smote.fit_resample(
+                    X_train_scaled, y_train)
+            else:
+                smote = SMOTEN(random_state=42)
+
+                X_train_res, y_train_res = smote.fit_resample(
+                    X_train_scaled, y_train
+                )
+        else:
+            raise ValueError("invalid sampling input")
+        
+        
+        print('After Sampling train_X: {}'.format(X_train_res.shape))
+        print('After Sampling train_y: {} \n'.format(y_train_res.shape))
+
+        
+        # numeric_processor=Pipeline(
+        #     steps=[("imputation_mean",SimpleImputer(missing_values=np.nan,strategy="mean")),
+        #            ("scaler",StandardScaler())]
+        # )
+
+        
+        
+        
+        # one hot 
+        cat_processor=make_pipeline(
+            OneHotEncoder(handle_unknown="ignore")            
+        )
+        cat_transformer = cat_processor if EncodeCat else "passthrough"
+        
+        Cat_transformation= ColumnTransformer([
+            ('cat', cat_transformer, cat_indices)],
+            remainder='passthrough')
+        
+        x_train_final = Cat_transformation.fit_transform(X_train_res)
+        x_test_final  = Cat_transformation.transform(X_test_scaled)
+        
+        return x_train_final,x_test_final,y_train_res,y_test
+    
+    
 
 
 
@@ -129,13 +275,19 @@ class Binclassification:
             X_train_res, y_train_res = rus.fit_resample(X_train_scaled, y_train)
             
         elif sampling==2:
-            
-            smote = SMOTENC(
-                categorical_features=cat_indices
+            if (1 in self.numcol):
+                
+                smote = SMOTENC(
+                    categorical_features=cat_indices
+                    )
+                X_train_res, y_train_res = smote.fit_resample(
+                    X_train_scaled, y_train)
+            else:
+                smote = SMOTEN(random_state=42)
+
+                X_train_res, y_train_res = smote.fit_resample(
+                    X_train_scaled, y_train
                 )
-            X_train_res, y_train_res = smote.fit_resample(
-                X_train_scaled, y_train)
-            
         else:
             raise ValueError("invalid sampling input")
         
@@ -184,7 +336,7 @@ class Binclassification:
                     }
                 ),
                 "SVC": (
-                    SVC(probability=True,max_iter=5000),
+                    SVC(probability=True,max_iter=1000),
                     {
                         'C': [0.1, 1, 10],
                         'gamma': [0.01, 0.1, 1],
@@ -383,3 +535,288 @@ class Binclassification:
     #     x_test_final  = Cat_transformation.transform(X_test_scaled)
     #     return x_train_final,x_test_final,y_train_res,y_test
     
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier, OutputCodeClassifier
+from sklearn.svm import SVC
+
+class MulticlassClassification:
+    def __init__(self,X,y,numcol,catcol):
+        self.X = X
+        self.y = y
+        self.numcol = numcol
+        self.catcol = catcol
+        self.simulation_result_n={}
+        self.simulation_result_one={}
+        self.data_sim_one={}
+        self.data_sim_n={}
+        self.validate_and_assign_columns()
+        
+
+    def validate_and_assign_columns(self):
+        numeric_cols = []
+        cat_cols = []
+
+        for Num, Cat, Col in zip(self.numcol, self.catcol, self.X.columns.tolist()):
+            if Num == 1 and Cat == 1:
+                raise Exception("Column cannot be both categorical and numeric")
+
+            if Num == 1:
+                numeric_cols.append(Col)
+            elif Cat == 1:
+                cat_cols.append(Col)
+            else:
+                raise Exception("Column must be either numeric or categorical")
+
+        self.numcol = numeric_cols
+        self.catcol = cat_cols
+    
+    def preprocess(self, EncodeCat=False,EncodeLabel=False,seed=42,sampling=2):
+        
+        '''
+        other logic would be apply one hot and std scaler the in last apply smote
+        then it would have 1 final pipline 
+        
+        '''
+
+        '''
+        Docstring for preprocess
+        
+        :param EncodeCat: by default false
+        :param EncodeLabel: by default false
+        :param seed: 
+        :param sampling: 0 for no sampling, 1 for undersampling, 2 for oversampling
+        '''
+        
+        # label encoding
+        if EncodeLabel:
+            self.label_encoder = LabelEncoder()
+            y_encoded = self.label_encoder.fit_transform(self.y)
+        else:
+            y_encoded = self.y
+        
+        X_train,X_test,y_train,y_test=train_test_split(self.X,y_encoded,random_state=seed,test_size=0.3, stratify=y_encoded)
+
+        
+        # standardization
+        numeric_pipeline = Pipeline(steps=[
+            ("imputer", SimpleImputer(strategy="mean")),
+            ("scaler", StandardScaler())
+        ])
+        # numeric_processor = make_pipeline(
+        #     SimpleImputer(strategy="mean"),
+        #     StandardScaler()
+        #     )
+        
+        scale_transformer = ColumnTransformer(
+            transformers=[
+            ("num", numeric_pipeline, self.numcol)
+            ],
+            remainder="passthrough"   # keep categorical untouched
+            )
+        
+        X_train_scaled = scale_transformer.fit_transform(X_train)
+        X_test_scaled  = scale_transformer.transform(X_test)
+        
+        # smotenc
+        original_cols = list(self.X.columns)
+        remainder_cols = [col for col in original_cols if col not in self.numcol]
+
+        cat_indices = [
+            len(self.numcol) + remainder_cols.index(cat_col)
+            for cat_col in self.catcol
+        ]
+
+        if sampling==0:
+            
+            X_train_res,y_train_res=X_train_scaled,y_train
+            
+        elif sampling==1:
+
+            rus = RandomUnderSampler(sampling_strategy='majority')
+            X_train_res, y_train_res = rus.fit_resample(X_train_scaled, y_train)
+            
+        elif sampling==2:
+            if (1 in self.numcol):
+                
+                smote = SMOTENC(
+                    categorical_features=cat_indices
+                    )
+                X_train_res, y_train_res = smote.fit_resample(
+                    X_train_scaled, y_train)
+            else:
+                smote = SMOTEN(random_state=42)
+
+                X_train_res, y_train_res = smote.fit_resample(
+                    X_train_scaled, y_train
+                )
+        else:
+            raise ValueError("invalid sampling input")
+        
+        
+        print('After Sampling train_X: {}'.format(X_train_res.shape))
+        print('After Sampling train_y: {} \n'.format(y_train_res.shape))
+
+        
+        # numeric_processor=Pipeline(
+        #     steps=[("imputation_mean",SimpleImputer(missing_values=np.nan,strategy="mean")),
+        #            ("scaler",StandardScaler())]
+        # )
+
+        
+        
+        
+        # one hot 
+        cat_processor=make_pipeline(
+            OneHotEncoder(handle_unknown="ignore")            
+        )
+        cat_transformer = cat_processor if EncodeCat else "passthrough"
+        
+        Cat_transformation= ColumnTransformer([
+            ('cat', cat_transformer, cat_indices)],
+            remainder='passthrough')
+        
+        x_train_final = Cat_transformation.fit_transform(X_train_res)
+        x_test_final  = Cat_transformation.transform(X_test_scaled)
+        
+        return x_train_final,x_test_final,y_train_res,y_test
+    
+    
+    def _run_simulation(self, EncodeCat, EncodeLabel, seed, sampling):
+        
+        X_train, X_test, y_train, y_test = self.preprocess(
+            EncodeCat, EncodeLabel, seed, sampling
+        )
+
+        results = {}
+        models={
+            "LogisticRegression_OvR": (
+                OneVsRestClassifier(LogisticRegression(max_iter=5000)),
+                {
+                    "estimator__C": np.logspace(-4, 4, 10)
+                }
+            ),
+
+            "SVC_OvR": (
+                OneVsRestClassifier(SVC(probability=True)),
+                {
+                    "estimator__C": [0.1, 1, 10],
+                    "estimator__gamma": [0.01, 0.1],
+                    "estimator__kernel": ["linear", "rbf"]
+                }
+            ),
+
+            "SVC_OvO": (
+                OneVsOneClassifier(SVC(probability=True)),
+                {
+                    "estimator__C": [0.1, 1, 10],
+                    "estimator__gamma": [0.01, 0.1],
+                    "estimator__kernel": ["linear", "rbf"]
+                }
+            ),
+
+            "SVC_ECOC": (
+                OutputCodeClassifier(SVC(probability=True)),
+                {
+                    "estimator__C": [0.1, 1, 10],
+                    "estimator__gamma": [0.01, 0.1],
+                    "estimator__kernel": ["linear", "rbf"],
+                    "code_size": [1.5, 2]
+                }
+            ),
+
+        }
+
+        for name, (model, params) in models.items():
+            print(f"Training {name}...")
+
+            grid = GridSearchCV(
+                estimator=model,
+                param_grid=params,
+                cv=StratifiedKFold(10),
+                scoring='accuracy',
+                n_jobs=-1,
+                verbose=1
+            )
+
+            grid.fit(X_train, y_train)
+            best_model = grid.best_estimator_
+
+            y_pred = best_model.predict(X_test)
+            # if hasattr(best_model, "predict_proba"):
+            #     y_prob = best_model.predict_proba(X_test)
+
+            # elif hasattr(best_model, "decision_function"):
+            #     y_prob = best_model.decision_function(X_test)
+
+            # else:
+            #     y_prob = best_model.predict(X_test)
+
+
+            acc = accuracy_score(y_test, y_pred)
+            recall = recall_score(y_test, y_pred, average="macro")
+            precision = precision_score(y_test, y_pred, average="macro")
+            f1 = f1_score(y_test, y_pred, average="macro")
+
+            classes = np.unique(y_train)
+            # y_test_bin = label_binarize(y_test, classes=classes)
+
+            # auc = roc_auc_score(
+            #     y_test_bin,
+            #     y_prob,
+            #     multi_class="ovr",
+            #     average="macro"
+            # )
+
+            cm = confusion_matrix(y_test, y_pred)
+
+            results[name] = {
+                "best_estimator": best_model,
+                "best_score": grid.best_score_,
+                "best_params": grid.best_params_,
+                "test_result": {
+                    "Accuracy": acc,
+                    "Recall (Macro)": recall,
+                    "Precision (Macro)": precision,
+                    "F1-score (Macro)": f1,
+                    # "AUC (OvR Macro)": auc,
+                    "ConfusionMatrix": cm
+                }
+            }
+
+        data = {
+            "X_train": X_train,
+            "X_test": X_test,
+            "y_train": y_train,
+            "y_test": y_test
+        }
+
+        return data, results
+    
+    def simulate_one(self, EncodeCat=True, EncodeLabel=True, seed=42, sampling=2):
+
+        data, results = self._run_simulation(
+                EncodeCat, EncodeLabel, seed, sampling
+        )
+
+        self.data_sim_one[f"seed {seed}"] = data
+        self.simulation_result_one[f"seed {seed}"] = results
+
+            
+            
+
+
+    
+    def simulate_ntimes(self, n=5, EncodeCat=True, EncodeLabel=True, sampling=2):
+
+        seeds = random.sample(range(1, 101), n)
+
+        for seed in seeds:
+
+            data, results = self._run_simulation(
+                EncodeCat, EncodeLabel, seed, sampling
+            )
+
+            self.data_sim_n[f"seed {seed}"] = data
+            self.simulation_result_n[f"seed {seed}"] = results
